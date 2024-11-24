bdac775cb18b21c73111486a7bd4650c05727566
Mountain26 <sonseo15072006@gmail.com>
2024-11-24 22:47:08 +0700

    Add files via upload

--- Session 4 BTVN 10.cpp
+++ Session 4 BTVN 10.cpp
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c;
-	
-	printf("Moi ban nhap vao so thu nhat: ");
+    int a, b, c;
+
+    printf("Moi ban nhap vao so thu nhat: ");
     scanf("%d", &a);
     printf("Moi ban nhap vao so thu hai: ");
     scanf("%d", &b);
     printf("Moi ban nhap vao so thu ba: ");
     scanf("%d", &c);
     
-    if(a > c && c > b){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", b, c, a);
-	}if(a > b && b > c){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", c, b, a);
-	}if(b > c && c > a){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", a, c, b);
-	}if(b > a && a > c){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", c, a, b);
-	}if(c > b && b > a){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", a, b, c);
-	}if(c > a && a > b){
-    	printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", b, a, c);
-	}
-	return 0;
+    if (a <= b && b <= c) {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", a, b, c);
+    } else if (a <= c && c <= b) {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", a, c, b);
+    } else if (b <= a && a <= c) {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", b, a, c);
+    } else if (b <= c && c <= a) {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", b, c, a);
+    } else if (c <= a && a <= b) {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", c, a, b);
+    } else {
+        printf("Cac so theo thu tu tu nho den lon la: %d %d %d\n", c, b, a);
+    }
+    
+    return 0;
 }
+
