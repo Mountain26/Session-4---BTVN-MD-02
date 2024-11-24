a7d66102acb5e14fb20942e56e879b5aecbf3223
Mountain26 <sonseo15072006@gmail.com>
2024-11-24 22:06:27 +0700

    Add files via upload

--- Session 4 BTVN 9.cpp
+++ Session 4 BTVN 9.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	
-    int day, month, year;
+    int day, month, year,soNgay;
     
     printf("Moi ban nhap vao ngay: ");
     scanf("%d", &day);
@@ -12,12 +12,33 @@ int main() {
     printf("Moi ban nhap vao ngay vao nam: ");
     scanf("%d", &year);
     
-    if (1 <= day && day <= 31 && month >= 1 && month <= 12) {
-        printf("Ngay thang nam hop le.\n");
-    } else {
-        printf("Ngay thang nam khong hop le.\n");
-    }
-
-    return 0;
+   if(year<1){
+		printf("Nam khong hop le");
+	}else if(month<1 || month>12){
+		printf("Thang khong hop le");
+	}else{
+		switch(month){
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+				soNgay = 31;
+			break;
+			case 4: case 6: case 9: case 11:
+				soNgay = 30;
+			break;
+			case 2:
+				if((year%4==0 && year%100!=0) || (year%400==0)){
+					soNgay = 29;
+				}else{
+					soNgay = 28;
+				}
+			break;
+		}
+	}
+	
+	if(day<1 || day>soNgay){
+		printf("%d/%d/%d khong hop le",day,month,year);
+	}else{
+		printf("%d/%d/%d hop le",day,month,year);
+	}
+	
+	return 0;
 }
-
